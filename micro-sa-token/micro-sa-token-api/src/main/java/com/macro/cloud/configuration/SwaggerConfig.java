b03b74093dbf6fda22256493f62b10560ace5aba
chenxiangde <xiangdechen@163.com>
2022-06-12 17:59:29 +0800

    swagger-ui

---
+++ micro-sa-token/micro-sa-token-api/src/main/java/com/macro/cloud/configuration/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.macro.cloud.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                //apiInfo指定测试文档基本信息，这部分将在页面展示
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    //基本信息，页面展示
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("受保护的API服务")
+                .description("受保护的API服务")
+                //版本号
+                .version("1.0.0")
+                .build();
+    }
+}
