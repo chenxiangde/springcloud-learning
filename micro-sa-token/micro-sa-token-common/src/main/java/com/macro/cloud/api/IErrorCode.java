72e4513b00b1c83f3bdfb90cd3bdd080d7317ac4
chenxiangde <xiangdechen@163.com>
2022-06-12 20:58:24 +0800

    swagger-ui

--- micro-sa-token/micro-sa-token-common/src/main/java/com/macro/cloud/api/IErrorCode.java
+++ micro-sa-token/micro-sa-token-common/src/main/java/com/macro/cloud/api/IErrorCode.java
@@ -2,9 +2,10 @@ package com.macro.cloud.api;
 
 /**
  * 封装API的错误码
- * Created by macro on 2019/4/19.
+ * Created by cxd.
  */
 public interface IErrorCode {
+
     long getCode();
 
     String getMessage();
--- micro-sa-token/micro-sa-token-common/src/main/java/com/macro/cloud/api/ResultCode.java
+++ micro-sa-token/micro-sa-token-common/src/main/java/com/macro/cloud/api/ResultCode.java
@@ -2,14 +2,16 @@ package com.macro.cloud.api;
 
 /**
  * 枚举了一些常用API操作码
- * Created by macro on 2019/4/19.
+ * Created by cxd.
  */
 public enum ResultCode implements IErrorCode {
+
     SUCCESS(200, "操作成功"),
     FAILED(500, "操作失败"),
     VALIDATE_FAILED(404, "参数检验失败"),
     UNAUTHORIZED(401, "暂未登录或token已经过期"),
     FORBIDDEN(403, "没有相关权限");
+
     private long code;
     private String message;
 
