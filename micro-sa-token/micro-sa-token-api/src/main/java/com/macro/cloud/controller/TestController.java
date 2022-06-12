fce788eb42a4328967876cd6f6cbe9ef13ab8a3b
chenxiangde <xiangdechen@163.com>
2022-06-12 18:11:27 +0800

    swagger-ui

--- micro-sa-token/micro-sa-token-api/src/main/java/com/macro/cloud/controller/TestController.java
+++ micro-sa-token/micro-sa-token-api/src/main/java/com/macro/cloud/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.macro.cloud.controller;
 
 import com.macro.cloud.api.CommonResult;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 测试接口
  * Created by macro on 2020/6/19.
  */
+@Api(tags = "TestController", description = "测试接口")
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -17,5 +19,4 @@ public class TestController {
     public CommonResult hello() {
         return CommonResult.success("Hello World.");
     }
-
 }
--- micro-sa-token/micro-sa-token-api/src/main/java/com/macro/cloud/controller/UserController.java
+++ micro-sa-token/micro-sa-token-api/src/main/java/com/macro/cloud/controller/UserController.java
@@ -3,14 +3,16 @@ package com.macro.cloud.controller;
 import cn.dev33.satoken.stp.StpUtil;
 import com.macro.cloud.api.CommonResult;
 import com.macro.cloud.domain.UserDTO;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 获取登录用户信息接口
- * Created by macro on 2020/6/19.
+ * Created by cxd.
  */
+@Api(tags = "UserController", description = "获取登录用户信息")
 @RestController
 @RequestMapping("/user")
 public class UserController{
@@ -20,5 +22,4 @@ public class UserController{
         UserDTO userDTO = (UserDTO) StpUtil.getSession().get("userInfo");
         return CommonResult.success(userDTO);
     }
-
 }
