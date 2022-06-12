b6b0a63dec595b13d177f0f5a7fcd6f04f63cacd
chenxiangde <xiangdechen@163.com>
2022-06-12 21:18:47 +0800

    swagger-ui

--- micro-sa-token/micro-sa-token-gateway/src/main/java/com/macro/cloud/common/GlobalExceptionHandler.java
+++ micro-sa-token/micro-sa-token-gateway/src/main/java/com/macro/cloud/common/GlobalExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.macro.cloud.common;
 
+import cn.dev33.satoken.exception.DisableLoginException;
 import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.exception.NotPermissionException;
+import cn.dev33.satoken.exception.NotRoleException;
 import com.macro.cloud.api.CommonResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 全局异常处理
@@ -13,13 +19,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class GlobalExceptionHandler {
  
-    /**
-     * 处理未登录的异常
-     */
+    // 全局异常拦截
     @ResponseBody
-    @ExceptionHandler(value = NotLoginException.class)
-    public CommonResult handleNotLoginException(NotLoginException e) {
-        return CommonResult.unauthorized(e.getMessage());
+    @ExceptionHandler(Exception.class)
+    public CommonResult handlerException(Exception e, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            if (e instanceof NotLoginException) {// 如果是未登录异常
+                NotLoginException ee = (NotLoginException) e;
+                return CommonResult.unauthorized(ee.getMessage());
+            } else if (e instanceof NotRoleException) {// 如果是角色异常
+                NotRoleException ee = (NotRoleException) e;
+                return CommonResult.forbidden(ee.getMessage());
+            } else if (e instanceof NotPermissionException) {// 如果是权限异常
+                NotPermissionException ee = (NotPermissionException) e;
+                return CommonResult.forbidden(ee.getMessage());
+            } else if (e instanceof DisableLoginException) {// 如果是被封禁异常
+                DisableLoginException ee = (DisableLoginException) e;
+                return CommonResult.forbidden(ee.getMessage());
+            } else {// 普通异常，输出：500 + 异常信息
+                return CommonResult.failed(e.getMessage());
+            }
+        } catch (Exception ex) {
+            return CommonResult.failed(e.getMessage());
+        }
     }
-
 }
